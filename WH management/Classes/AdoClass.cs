d2797abd7ac4cd3fe49fc72e8538ea82c5b7a097
Khaled Eid <khaled_eid77@outlook.com>
2024-06-26 06:44:27 +0300

    Create The project and add set the connection with database

---
+++ WH management/Classes/AdoClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WH_management.Classes
+{
+    public class AdoClass
+    {
+        public static SqlConnection sqlcn;
+
+        public static void setConnection()
+        {
+            sqlcn = new SqlConnection(WH_management.Properties.Settings.Default.connection);
+        }
+    }
+}
---
+++ WH management/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WH_management.Classes;
+
+namespace WH_management
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            AdoClass.setConnection();
+            Application.Run(new mainForm());
+        }
+    }
+}
